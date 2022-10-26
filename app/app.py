99b1dbf6217b2e8693460329021d468552bde86f
Nanao Ei <nanaoei@protonmail.com>
2022-10-26 17:39:32 -0400

    fix lint

--- app/app.py
+++ app/app.py
@@ -18,44 +18,48 @@ from promptopt import utils
 
 embedding_model = embed.CLIP()
 
-net_arch_kwargs = {
-  'n_layers': 2,
-  'layer_size': 256
-}
+net_arch_kwargs = {"n_layers": 2, "layer_size": 256}
 pref_model_train_config = rlhf.get_default_config()
 pref_model_train_config.verbose = True
 
+
 def optimize_prompt(data):
-    prompts = data['prompts']
-    pref_data = data['prefs']
-    pref_model = models.PrefModel(embedding_model.n_embedding_dims, net_arch_kwargs=net_arch_kwargs)
+    prompts = data["prompts"]
+    pref_data = data["prefs"]
+    pref_model = models.PrefModel(
+        embedding_model.n_embedding_dims, net_arch_kwargs=net_arch_kwargs
+    )
     embeddings = embedding_model.embed_strings(prompts)
     embedding_dataset = datasets.EmbeddingDataset(embeddings=list(embeddings))
     pref_dataset = datasets.PrefDataset(embedding_dataset, pref_data=pref_data)
-    optimizer = rlhf.RLHF(pref_model, pref_model_train_config, embedding_dataset, pref_dataset)
+    optimizer = rlhf.RLHF(
+        pref_model, pref_model_train_config, embedding_dataset, pref_dataset
+    )
     candidate_scores = np.array(pref_model.score(embeddings))
     scored_candidates = list(zip(prompts, candidate_scores))
     init_prompt = max(scored_candidates, key=lambda x: x[1])[0]
     score_func = lambda x: torch.tensor(np.array(pref_model.score(x)))
     gator = interrogator.Gator(embedding_model=embedding_model, score_func=score_func)
-    #best_prompt = gator.search(init_prompt)
-    best_prompt = init_prompt # DEBUG
+    # best_prompt = gator.search(init_prompt)
+    best_prompt = init_prompt  # DEBUG
     return best_prompt
+
 
 app = Flask(__name__, template_folder=utils.TEMPLATE_DIR)
 
-@app.route('/api/optimize', methods=['POST'])
+
+@app.route("/api/optimize", methods=["POST"])
 def search():
-  data = json.loads(request.values['data'])
-  prompt = optimize_prompt(data)
-  resp = {'optimized_prompt': prompt}
-  return json.dumps(resp)
+    data = json.loads(request.values["data"])
+    prompt = optimize_prompt(data)
+    resp = {"optimized_prompt": prompt}
+    return json.dumps(resp)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def main():
-  return render_template('index.html')
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=utils.FLASK_PORT)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=utils.FLASK_PORT)
